6dddc7c5fc5be3d43f2663c1296592201bac0335
Marcos Santos <marcossantos8002@gmail.com>
2023-11-12 20:07:55 -0300

    test(post.js / swagger.js): created Route Documentation Test with swagger library

---
+++ swagger.js
@@ -0,0 +1,17 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Nome da Sua API',
+      version: '1.0.0',
+      description: 'Descrição da Sua API',
+    },
+  },
+  apis: ['./routes/*.js'], // Adapte o padrão de arquivos de rota conforme necessário
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
